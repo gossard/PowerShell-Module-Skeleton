9f16c4106749a971d3d216f8d2ebfa69bbdfc698
Florent Gossard <florentgossard@gmail.com>
2023-11-13 09:17:54 +0100

    First Commit

---
+++ Build.ps1
@@ -0,0 +1,32 @@
+function Get-SortedPs1Files([string]$FolderPath) {
+    Get-ChildItem $FolderPath -Filter '*.ps1' | Sort-Object 'Name'
+}
+
+[string]$Encoding = "utf8"
+
+[string]$ModuleName = "MyModule"
+[string]$ModulePath   = Join-Path $PSScriptRoot -ChildPath "$($ModuleName).psm1"
+[string]$ManifestPath = Join-Path $PSScriptRoot -ChildPath "$($ModuleName).psd1"
+
+$Classes          = Get-SortedPs1Files (Join-Path $PSScriptRoot -ChildPath "Code\Classes")
+$PublicFunctions  = Get-SortedPs1Files (Join-Path $PSScriptRoot -ChildPath "Code\Functions\Public")
+$PrivateFunctions = Get-SortedPs1Files (Join-Path $PSScriptRoot -ChildPath "Code\Functions\Private")
+$PreContent       = Get-Item           (Join-Path $PSScriptRoot -ChildPath "Code\PreContent.ps1")
+$PostContent      = Get-Item           (Join-Path $PSScriptRoot -ChildPath "Code\PostContent.ps1")
+
+"# Built on $(Get-Date)" | Out-File $ModulePath -Encoding $Encoding
+
+$ModuleFiles = @()
+$ModuleFiles += $PreContent
+$ModuleFiles += $Classes
+$ModuleFiles += $PublicFunctions
+$ModuleFiles += $PrivateFunctions
+$ModuleFiles += $PostContent
+
+$ModuleFiles | Get-Content | Out-File $ModulePath -Encoding $Encoding -Append
+
+if($null -eq $PublicFunctions) {
+    Update-ModuleManifest $ManifestPath -FunctionsToExport '*'
+} else {
+    Update-ModuleManifest $ManifestPath -FunctionsToExport $PublicFunctions.BaseName
+}
