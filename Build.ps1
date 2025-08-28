4bfeeb9b4be9e9f3a27f17f730042f74866e8e79
Florent Gossard <florentgossard@gmail.com>
2025-08-28 11:53:28 +0200

    refactor(build): Improve file reading/writing logic and readability
    - File I/O now uses .NET's `[System.IO.File]` with an explicit UTF-8 (No BOM) encoding
    - Adds `# --- Content from ---` separators to the final module file.

--- Build.ps1
+++ Build.ps1
@@ -2,7 +2,7 @@ function Get-SortedPs1Files([string]$FolderPath) {
     Get-ChildItem $FolderPath -Filter '*.ps1' -File -Recurse | Sort-Object 'Name'
 }
 
-[string]$Encoding = "utf8"
+[System.Text.UTF8Encoding]$TargetEncoding = [System.Text.UTF8Encoding]::new($false)
 
 [string]$ModuleName = "MyModule"
 [string]$ModulePath   = Join-Path $PSScriptRoot -ChildPath "$($ModuleName).psm1"
@@ -14,16 +14,25 @@ $PrivateFunctions = Get-SortedPs1Files (Join-Path $PSScriptRoot -ChildPath "Code
 $PreContent       = Get-Item           (Join-Path $PSScriptRoot -ChildPath "Code\PreContent.ps1")
 $PostContent      = Get-Item           (Join-Path $PSScriptRoot -ChildPath "Code\PostContent.ps1")
 
-"# Built on $(Get-Date)" | Out-File $ModulePath -Encoding $Encoding
+$SourceFiles = @()
+$SourceFiles += $PreContent
+$SourceFiles += $Classes
+$SourceFiles += $PublicFunctions
+$SourceFiles += $PrivateFunctions
+$SourceFiles += $PostContent
 
-$ModuleFiles = @()
-$ModuleFiles += $PreContent
-$ModuleFiles += $Classes
-$ModuleFiles += $PublicFunctions
-$ModuleFiles += $PrivateFunctions
-$ModuleFiles += $PostContent
+[System.Text.StringBuilder]$ModuleContent = [System.Text.StringBuilder]::new()
 
-$ModuleFiles | Get-Content | Out-File $ModulePath -Encoding $Encoding -Append
+[void]$ModuleContent.AppendLine("# Built on $(Get-Date)")
+
+foreach($File in $SourceFiles) {
+    [void]$ModuleContent.AppendLine([string]::Empty)
+    [void]$ModuleContent.AppendLine("# --- Content from $($File.Name) ---")
+    [void]$ModuleContent.AppendLine([string]::Empty)
+    [void]$ModuleContent.AppendLine([System.IO.File]::ReadAllText($File.FullName))
+}
+
+[System.IO.File]::WriteAllText($ModulePath, $ModuleContent.ToString(), $TargetEncoding)
 
 if($null -eq $PublicFunctions) {
     Update-ModuleManifest $ManifestPath -FunctionsToExport '*'
